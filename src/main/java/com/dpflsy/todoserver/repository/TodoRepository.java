bad2932e95fe03b9c12f01ab6da0511bac48b1df
dpflsy25 <dpflsy25@gmail.com>
2022-08-07 19:47:06 +0900

    todoList 첫 commit

---
+++ src/main/java/com/dpflsy/todoserver/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.dpflsy.todoserver.repository;
+
+import com.dpflsy.todoserver.model.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
+}
